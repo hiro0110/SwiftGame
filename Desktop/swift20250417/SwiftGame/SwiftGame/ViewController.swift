455a09f720a85a4833813b9c63db766cedb19ded
student <student@TSA25-GAME05.local>
2025-05-01 13:12:23 +0900

    初回コミット

---
+++ Desktop/swift20250417/SwiftGame/SwiftGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftGame
+//
+//  Created by student on 2025/05/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
